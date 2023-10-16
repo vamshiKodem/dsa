3e8ee98e4e61e9464a9f24e6715c66cb5b57561f
vamshikrishnakodem95 <vamshikdm@gmail.com>
2023-10-16 15:58:23 +0530

    added polyfill for promise.resolve, reject and all

--- polyfills.js
+++ polyfills.js
@@ -41,6 +41,85 @@ Array.prototype.myReduce = function (cb, initialValue) {
   return acc;
 };
 
-console.log(number.myReduce((acc, curr) => acc + curr, 0));
+// console.log(number.myReduce((acc, curr) => acc + curr, 0));
 
-console.log(number.reduce((acc, curr) => acc + curr, 0));
+// console.log(number.reduce((acc, curr) => acc + curr, 0));
+
+// Polyfill for settimeout
+function timeoutWrapper() {
+  let id = 0;
+  const timerHash = {};
+
+  function mySetTimeout(cb, delay, ...args) {
+    id++;
+    timerHash[id] = true;
+    const start = Date.now();
+    function triggerCallback() {
+      if (!timerHash[id]) return;
+      if (Date.now() >= start + delay) {
+        cb.apply(this, args);
+      } else {
+        requestIdleCallback(triggerCallback);
+      }
+    }
+    triggerCallback(triggerCallback);
+    return id;
+  }
+
+  function myClearTimeout(id) {
+    delete timerHash[id];
+  }
+
+  return {
+    myClearTimeout,
+    mySetTimeout,
+  };
+}
+
+const { myClearTimeout, mySetTimeout } = timeoutWrapper();
+
+// const myId = mySetTimeout((...props) => {
+//   console.log("My set time out", ...props);
+// }, 1000);
+
+//   myClearTimeout(myId);
+
+// Polyfill for Promise.resolve
+const promiseResolve = (value) => {
+  return new Promise((resolve) => resolve(value));
+};
+
+// Polyfill for Promise.reject
+const promiseReject = (value) => {
+  return new Promise((_resolve, reject) => reject(value));
+};
+
+// Polyfill for Promise.all
+
+const promiseAll = (promises) => {
+  return new Promise((resolve, reject) => {
+    const output = [];
+    promises.forEach((promise, index) => {
+      promise
+        .then((data) => {
+          output.push(data);
+          if (promises.length - 1 === index) {
+            resolve(output);
+          }
+        })
+        .catch((error) => {
+          reject(error);
+        });
+    });
+  });
+};
+
+promiseAll([
+  Promise.resolve(10),
+  Promise.resolve(20),
+  Promise.reject("not working"),
+])
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((data) => console.log(data));
