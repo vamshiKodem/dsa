141fa158c41db7424b7e5ec3a58675d4d016e4a8
vamshikrishnakodem95 <vamshikdm@gmail.com>
2023-10-22 18:57:50 +0530

    added throttling function

---
+++ throttling.js
@@ -0,0 +1,22 @@
+const primaryButton = document.querySelector(".primary-button");
+
+function throttling(cb, delay) {
+  const context = this;
+  let timer;
+
+  return function (...args) {
+    if (timer) {
+      return;
+    }
+    timer = setTimeout(() => {
+      cb.apply(context, args);
+      timer = undefined;
+    }, delay);
+  };
+}
+
+function onPrimaryButtonClick() {
+  console.log("primary button is clicked");
+}
+
+primaryButton.addEventListener("click", throttling(onPrimaryButtonClick, 500));
