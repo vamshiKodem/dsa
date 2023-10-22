649256e81b83e7c0419fdd684f6d866763288220
vamshikrishnakodem95 <vamshikdm@gmail.com>
2023-10-22 18:42:02 +0530

    added debounce

---
+++ debouncing.js
@@ -0,0 +1,20 @@
+const textInput = document.querySelector(".text-input");
+
+function debouncing(cb, delay) {
+  const context = this;
+  let timer;
+  return function (...args) {
+    if (timer) {
+      clearTimeout(timer);
+    }
+    timer = setTimeout(() => {
+      cb.apply(context, args);
+    }, delay);
+  };
+}
+
+function callback() {
+  console.log("I am callback function");
+}
+
+textInput.addEventListener("keyup", debouncing(callback, 500));
