6190ed90add2877b72e92ea1e193a68cd8089927
Vamshi Krishna Kodem <139354055+vamshiKodem@users.noreply.github.com>
2023-12-27 12:12:18 +0530

    Update LC_88.js

--- dsa/array/LC_88.js
+++ dsa/array/LC_88.js
@@ -4,7 +4,7 @@ const merge = (nums1, m, nums2, n) => {
   while (n) {
     // here we compare nums1(without zero's) and nums2 last value
     if (nums1[m - 1] > nums2[n - 1]) {
-      // if nums1 is greater than nums2
+      // if nums1 is greater than nums2 and assign that to last character of nums1 (including zero's)
       nums1[m + n - 1] = nums1[m - 1];
       m--;
     } else {
