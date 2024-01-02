6b271cb3dd01b11fce0d2daa5ab8a6a20efbadb2
vamshikrishnakodem95 <vamshikdm@gmail.com>
2024-01-02 11:29:40 +0530

    leetcode 55

---
+++ dsa/array/LC_55.js
@@ -0,0 +1,15 @@
+// 55. Jump Game
+const canJump = (nums) => {
+  let step = nums[0];
+  for (let i = 1; i < nums.length; i++) {
+    step--;
+    if (step < 0) {
+      return false;
+    } else if (nums[i] > step) {
+      step = nums[i];
+    }
+  }
+  return true;
+};
+
+canJump([2, 3, 1, 1, 4]);
