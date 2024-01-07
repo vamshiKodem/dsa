fe178b47d6dc49d60d79ce2c838e0134c0b8cd59
vamshikrishnakodem95 <vamshikdm@gmail.com>
2024-01-07 13:07:26 +0530

    leetcode 167

---
+++ dsa/array/LC_167.js
@@ -0,0 +1,17 @@
+// 167. Two Sum II - Input Array Is Sorted
+const twoSum = (nums, target) => {
+  let left = 0;
+  let right = nums.length - 1;
+  while (left < right) {
+    const sum = nums[left] + nums[right];
+    if (sum < target) {
+      left++;
+    } else if (sum > target) {
+      right--;
+    } else {
+      return [left + 1, right + 1];
+    }
+  }
+};
+
+twoSum([2, 7, 11, 15], 7);
