43d8bca513647eaf57d2ff0d2ec8ba1005e23d71
vamshiKodem <vamshikrishna.kodem96@gmail.com>
2024-05-01 13:33:32 +0530

    added find the second largest number in array

---
+++ dsa/array/findSecondLargest.js
@@ -0,0 +1,17 @@
+const numbers = [20, 10, 3, 200, 59, 100];
+
+const findSecondLargest = (numbers) => {
+  let larger = -Infinity;
+  let smaller = -Infinity;
+  for (let i = 0; i < numbers.length; i++) {
+    if (larger < numbers[i]) {
+      smaller = larger;
+      larger = numbers[i];
+    } else if (smaller < numbers[i]) {
+      smaller = numbers[i];
+    }
+  }
+  return smaller;
+};
+
+console.log(findSecondLargest(numbers));
