515af4c598d1892c166477e4bfb281b0132c86af
Vamshi Krishna Kodem <139354055+vamshiKodem@users.noreply.github.com>
2024-06-26 12:13:10 +0530

    Update interviewQuestions.js

--- questions/interviewQuestions.js
+++ questions/interviewQuestions.js
@@ -44,3 +44,44 @@ const sumCalculate = (data) => {
 };
 
 console.log(sumCalculate(data));
+
+
+const arr = [
+  "He said he was not there yesterday; however, many people saw him there.",
+  "The small white buoys marked the location of hundreds of crab pots.",
+  "My uncle's favourite pastime was building cars out of noodles.",
+  "The beauty of the African sunset disguised the danger lurking nearby.",
+  "There aren't enough towels in the world to stop the sewage flowing from his mouth.",
+  "He swore he just saw his sushi move.",
+  "The light that burns twice as bright burns half as long.",
+  "Potato wedges probably are not best for relationships.",
+  "When he asked her favourite number, she answered without hesitation that it was diamonds.",
+  "When he had to picnic on the beach, he purposely put sand in other people food.",
+];
+
+const pick = (arr) => {
+  const hash = {};
+  for (let i = 0; i < arr.length; i++) {
+    const arrStr = arr[i].split(" ");
+    for (let j = 0; j < arrStr.length; j++) {
+      if (hash[arrStr[i]]) {
+        hash[arrStr[i]]++;
+      } else {
+        hash[arrStr[i]] = 1;
+      }
+    }
+  }
+
+  const topFive = [];
+  let word = [];
+  for (key in hash) {
+    word.push({ word: key, times: hash[key] });
+  }
+  word = word.sort((a, b) => b.times - a.times);
+  word.forEach((val, i) => {
+    if (i <= 4) {
+      topFive.push(val);
+    }
+  });
+  return topFive;
+};
