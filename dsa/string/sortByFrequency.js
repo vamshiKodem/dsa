11522d094073d64624248befbd60d7fadf65e066
Vamshi Krishna Kodem <139354055+vamshiKodem@users.noreply.github.com>
2024-11-21 15:37:36 +0530

    Update sortByFrequency.js

--- dsa/string/sortByFrequency.js
+++ dsa/string/sortByFrequency.js
@@ -1,1 +1,23 @@
+const str = 'helloworld';
+
+const find = str => {
+  const obj = {};
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] in obj) {
+      obj[str[i]]++;
+    } else {
+      obj[str[i]] = 1;
+    }
+  }
+  const sortedArray = Object.keys(obj).sort((a, b) => {
+    return obj[b] - obj[a];
+  });
+  let ouput = '';
+  for (let i = 0; i < sortedArray.length; i++) {
+    ouput = ouput + sortedArray[i];
+  }
+  return ouput;
+};
+
+console.log(find(str));
 
